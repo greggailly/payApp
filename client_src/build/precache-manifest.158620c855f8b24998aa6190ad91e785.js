b736e8968a6381b771a5a1e906ae670d3dbbd22e
Greg <greggailly@yahoo.fr>
2019-05-04 14:58:54 +0200

    Revert "before new branch"
    This reverts commit 542849e5810d3bf98cd82b50d42000a836c7facd.

---
+++ client_src/build/precache-manifest.158620c855f8b24998aa6190ad91e785.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b6072b8e6fcb89c1cce",
+    "url": "/static/css/main.5975152b.chunk.css"
+  },
+  {
+    "revision": "6b6072b8e6fcb89c1cce",
+    "url": "/static/js/main.240e47d6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7c956465b2dcae8abd49",
+    "url": "/static/js/2.922483cc.chunk.js"
+  },
+  {
+    "revision": "4db02590418134fb6126a0926b668af1",
+    "url": "/index.html"
+  }
+];
