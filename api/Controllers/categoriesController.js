68006814511529f6244cef1110db9cc237d880f9
Greg <greggailly@yahoo.fr>
2019-05-06 12:47:23 +0200

    categories results

--- api/Controllers/categoriesController.js
+++ api/Controllers/categoriesController.js
@@ -5,7 +5,9 @@ exports.create = (req, res, next) => {
     const category = new Category()
     category.save()
         .then(result => {
-            res.status(200).json({})
+            res.status(200).json({
+                createdCategory: result
+            })
         })
         .catch(err => {
             throw new Error(err)
@@ -15,7 +17,9 @@ exports.create = (req, res, next) => {
 exports.readAll = (req, res, next) => {
     Category.find()
         .then(result => {
-            res.status(200).json({})
+            res.status(200).json({
+                categories: result
+            })
         })
         .catch(err => {
             throw new Error(err)
@@ -25,7 +29,9 @@ exports.readAll = (req, res, next) => {
 exports.read = (req, res, next) => {
     Category.findById()
         .then(result => {
-            res.status(200).json({})
+            res.status(200).json({
+                category: result
+            })
         })
         .catch(err => {
             throw new Error(err)
@@ -39,7 +45,9 @@ exports.update = (req, res, next) => {
             return category.save()
         })
         .then(result => {
-            res.status(200).json({})
+            res.status(200).json({
+                updatedCategory: result
+            })
         })
         .catch(err => {
             throw new Error(err)
