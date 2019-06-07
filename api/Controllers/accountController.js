acf07a314af1bf9de7639f942e659c4839b09ced
Greg <greggailly@yahoo.fr>
2019-06-07 11:56:24 +0200

    add async to account & auth controllers

--- api/Controllers/accountController.js
+++ api/Controllers/accountController.js
@@ -1,76 +1,71 @@
 const Account = require('../Models/account')
 const mongoose = require('mongoose')
 
-exports.create = (req, res, next) => {
+exports.create = async (req, res, next) => {
     const account = new Account({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         value: req.body.value
     })
-    account.save()
-        .then(result => {
-            res.status(200).json({
-                message: "Account created",
-                account: account
-            })
+    try {
+        const result = await account.save()
+        res.status(200).json({
+            message: "Account created",
+            account: account
         })
-        .catch(err => {
-            throw new Error(err)
-        })
+    } catch (error) {
+        throw new Error(error)
+    }
 }
 
-exports.readAll = (req, res, next) => {
-    Account.find()
-        .then(result => {
-            res.status(200).json({
-                accounts: result
-            })
+exports.readAll = async (req, res, next) => {
+    try {
+        const result = await Account.find()
+        res.status(200).json({
+            accounts: result
         })
-        .catch(err => {
-            throw new Error(err)
-        })
+    } catch (error) {
+        throw new Error(error)
+    }
 }
 
-exports.read = (req, res, next) => {
+exports.read = async (req, res, next) => {
     const id = req.params.id
-    Account.findById(id)
-        .then(result => {
-            res.status(200).json({
-                account: result
-            })
+    try {
+        const result = await Account.findById(id)
+        res.status(200).json({
+            account: result
         })
-        .catch(err => {
-            throw new Error(err)
-        })
+    } catch (error) {
+        throw new Error(error)
+    }
 }
 
-exports.update = (req, res, next) => {
+exports.update = async (req, res, next) => {
     const id = req.params.id
-    Account.findById(id)
-        .then(account => {
-            account.name = req.body.name
-            account.value = req.body.value
-            return account.save()
+    try {
+        const account = await Account.findById(id)
+        account.name = req.body.name
+        account.value = req.body.value
+        const result = await account.save()
+        res.status(200).json({
+            message: "Account updated",
+            account: result
         })
-        .then(result => {
-            res.status(200).json({
-                message: "Account updated",
-                account: result
-            })
-        })
-        .catch(err => {
-            throw new Error(err)
-        })
+    } catch (error) {
+        throw new Error(error)
+    }
 }
 
-exports.delete = (req, res, next) => {
-    Account.findOneAndRemove()
-        .then(result => {
-            res.status(200).json({
-                message: 'Delete Successfull'
-            })
+
+exports.delete = async (req, res, next) => {
+    const id = req.params.id
+    try {
+        const result = await Account.findById(id).remove()
+        res.status(200).json({
+            message: 'Delete Successfull'
         })
-        .catch(err => {
-            throw new Error(err)
-        })
+    } catch (error) {
+        throw new Error(error)
+    }
 }
--- api/Controllers/authController.js
+++ api/Controllers/authController.js
@@ -2,7 +2,7 @@ const User = require('../Models/user')
 const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
 
-exports.signup = (req, res, next) => {
+exports.signup = async (req, res, next) => {
     user = new User({
         _id: new mongoose.Types.ObjectId(),
         username: req.body.username,
@@ -10,49 +10,43 @@ exports.signup = (req, res, next) => {
         solde: 0,
         isAdmin: req.body.isAdmin
     })
-    return user.save()
-        .then(user => {
-            res.status(201).json({
-                message: "User created",
-                createdUser: user
-            })
+    try {
+        const user = await user.save()
+        res.status(201).json({
+            message: "User created",
+            createdUser: user
         })
-        .catch(err => {
-            console.log(err)
-        })
+    } catch (error) {
+        throw new Error(error)
+    }
 }
 
-exports.login = (req, res, next) => {
+exports.login = async (req, res, next) => {
     const badge = req.body.badge
-    User.findOne({ badge: badge })
-        .exec()
-        .then(user => {
-            if (!user) {
-                return res.status(401).json({
-                    message: 'Auth failed'
-                })
+    try {
+        const user = await User.findOne({ badge: badge }).exec()
+        if (!user) {
+            return res.status(401).json({
+                message: 'Auth failed'
+            })
+        }
+        const token = jwt.sign(
+            {
+                username: user.username,
+                userId: user._id
+            },
+            "secret",
+            {
+                expiresIn: "1h"
             }
-            const token = jwt.sign(
-                {
-                    username: user.username,
-                    userId: user._id
-                },
-                "secret",
-                {
-                    expiresIn: "1h"
-                }
-            )
-            return res.status(200).json({
-                message: "Auth successful ",
-                token: token,
-                user: user,
-                expiresIn: 3600
-            })
+        )
+        return res.status(200).json({
+            message: "Auth successful ",
+            token: token,
+            user: user,
+            expiresIn: 3600
         })
-        .catch(err => {
-            return res.status(403).json({
-                message: "error",
-                error: err
-            })
-        })
+    } catch (error) {
+        throw new Error(error)
+    }
 }
